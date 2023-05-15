ff8080bef91d970275948d4ddbef28c2ae2b56e1
probzyg <kaveckisj@gmail.com>
2023-05-15 03:17:39 +0300

    @flowcontrol switch -> enhanced switch

--- src/main/java/io/codelex/flowcontrol/practice/PhoneKeyPad.java
+++ src/main/java/io/codelex/flowcontrol/practice/PhoneKeyPad.java
@@ -7,47 +7,16 @@ public class PhoneKeyPad {
         Scanner input = new Scanner(System.in);
         System.out.print("Input a string you want to convert: ");
         String[] inputString = input.nextLine().toLowerCase().split("");
-        for (int i = 0; i < inputString.length; i++) {
-            switch (inputString[i]) {
-                case "a":
-                case "b":
-                case "c":
-                    System.out.print(2);
-                    break;
-                case "d":
-                case "e":
-                case "f":
-                    System.out.print(3);
-                    break;
-                case "g":
-                case "h":
-                case "i":
-                    System.out.print(4);
-                    break;
-                case "j":
-                case "k":
-                case "l":
-                    System.out.print(5);
-                    break;
-                case "m":
-                case "n":
-                case "o":
-                    System.out.print(6);
-                    break;
-                case "p":
-                case "q":
-                case "r":
-                case "s":
-                    System.out.print(7);
-                    break;
-                case "t":
-                case "u":
-                case "v":
-                    System.out.print(8);
-                    break;
-                default:
-                    System.out.print(9);
-                    break;
+        for (String s : inputString) {
+            switch (s) {
+                case "a", "b", "c" -> System.out.print(2);
+                case "d", "e", "f" -> System.out.print(3);
+                case "g", "h", "i" -> System.out.print(4);
+                case "j", "k", "l" -> System.out.print(5);
+                case "m", "n", "o" -> System.out.print(6);
+                case "p", "q", "r", "s" -> System.out.print(7);
+                case "t", "u", "v" -> System.out.print(8);
+                default -> System.out.print(9);
             }
         }
 
--- src/main/java/io/codelex/flowcontrol/practice/PrintDayInWord.java
+++ src/main/java/io/codelex/flowcontrol/practice/PrintDayInWord.java
@@ -4,29 +4,14 @@ public class PrintDayInWord {
     public static void main(String[] args) {
         int day = 1;
         switch (day) {
-            case 0:
-                System.out.println("Sunday");
-                break;
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            default:
-                System.out.println("Not a valid day");
+            case 0 -> System.out.println("Sunday");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            default -> System.out.println("Not a valid day");
         }
     }
 }

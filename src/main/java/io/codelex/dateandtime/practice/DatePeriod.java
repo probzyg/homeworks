c4c84ca46dabb331eeac5e7d393fb5c44388f023
probzyg <kaveckisj@gmail.com>
2023-05-29 19:31:46 +0300

    DatePeriod mistakes corrected

--- src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
+++ src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
@@ -23,7 +23,10 @@ public class DatePeriod {
         String overlapMessage = doesPeriodCompletelyInclude(otherPeriod);
 
         if (overlapMessage != null) {
-            System.out.println(overlapMessage);
+            LocalDate intersectionStart = getMaxStartDate(otherPeriod);
+            LocalDate intersectionEnd = getMinEndDate(otherPeriod);
+            DatePeriod overlap = new DatePeriod(intersectionStart, intersectionEnd);
+            System.out.println(overlapMessage + " Overlap: " + overlap.getStart() + "-" + overlap.getEnd());
             return null;
         } else if (!doPeriodsOverlap(otherPeriod)) {
             return null;
@@ -48,9 +51,9 @@ public class DatePeriod {
 
     private String doesPeriodCompletelyInclude(DatePeriod otherPeriod) {
         if (start.isBefore(otherPeriod.getStart()) && end.isAfter(otherPeriod.getEnd())) {
-            return "Overlap: first period completely includes the second";
+            return "First period completely includes the second!";
         } else if (otherPeriod.getStart().isBefore(start) && otherPeriod.getEnd().isAfter(end)) {
-            return "Overlap: second period completely includes the first";
+            return "Second period completely includes the first!";
         } else {
             return null;
         }
@@ -62,7 +65,7 @@ public class DatePeriod {
         DatePeriod firstPeriod = new DatePeriod(firstStart, firstEnd);
 
         LocalDate secondStart = LocalDate.of(2022, 1, 10);
-        LocalDate secondEnd = LocalDate.of(2022, 1, 25);
+        LocalDate secondEnd = LocalDate.of(2022, 1, 13);
         DatePeriod secondPeriod = new DatePeriod(secondStart, secondEnd);
 
         DatePeriod overlap = firstPeriod.intersection(secondPeriod);
